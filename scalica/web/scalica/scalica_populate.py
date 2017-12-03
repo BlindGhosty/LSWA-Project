df62758f493c6977157424f4b88ebb411ccdb420
kpwill <kpw243@nyu.edu>
2017-12-03 17:23:48 -0500

    populate scalica w variable number of new users

--- scalica/web/scalica/scalica_populate.py
+++ scalica/web/scalica/scalica_populate.py
@@ -8,33 +8,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-users = [
-	('b1','1'),
-	('b2','1'),
-	('b3','1'),
-	('b4','1'),
-	('b5','1'),
-	('b6','1'),
-	('b7','1'),
-	('b8','1'),
-	('b9','1'),
-	('b10','1'),
-	('b11','1'),
-	('b12','1'),
-	('b13','1'),
-	('b14','1'),
-	('b15','1'),
-	('b16','1'),
-	('b17','1'),
-	('b18','1'),
-	('b19','1'),
-	('b20','1'),
-	('b21','1'),
-	('b22','1'),
-	('b23','1'),
-	('b24','1'),
-	('b25','1'),
-]
+users = []
+
+for i in range(100):
+	newusername = 'b' + str(i)
+	users.append((newusername,'1'))
 
 for username, password in users:
 	try:
