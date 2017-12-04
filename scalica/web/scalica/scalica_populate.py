1b6920cfce60bfd61d1b3392500ecee2a397342b
mdb536 <mdb536@nyu.edu>
2017-12-04 16:46:57 +0000

    basic version for follows

--- scalica/web/scalica/scalica_populate.py
+++ scalica/web/scalica/scalica_populate.py
@@ -28,6 +28,7 @@ for username, password in users:
 		print 'User {0} successfully created.'.format(username)
 	except:
 		print 'There was a problem creating the user: {0}. Error:{1}.'.format(username, sys.exc_info()[1])
+
 print 'Finished populating user list'
 
 
@@ -36,65 +37,25 @@ followee_arr = []
 followee_str = ""
 follower_str = ""
 for i in range(6):
-	follower_str = "b" + str(random.randint(0,99))
+	follower_str = "seed" + str(random.randint(0,19))
 	while follower_str in follower_arr:
-		follower_str = "b" + str(random.randint(0,99))
-
-	followee_str = "b" + str(random.randint(0,99))
-	while followee_str in followee_arr || followee_str in follower_arr:
-		followee_str = "b" + str(random.randint(0,99))
-
+		follower_str = "seed" + str(random.randint(0,19))
+	followee_str = "seed" + str(random.randint(0,19))
+	while followee_str in followee_arr or followee_str in follower_arr:
+		followee_str = "seed" + str(random.randint(0,19))
 	follower_arr.append(follower_str)
 	followee_arr.append(followee_str)
+	print follower_str + "->" + followee_str
 
-follower_str = "b" + str(random.randint(0,99))
-while follower_str in follower_arr:
-	follower_str = "b" + str(random.randint(0,99))
-
-followee_str = "b" + str(random.randint(0,99))
-while followee_str in followee_arr || followee_str in follower_arr:
-	followee_str = "b" + str(random.randint(0,99))
-follower_arr.append(follower_str)
-followee_arr.append(followee_str)
-
-
-followee_str = "b" + str(random.randint(0,99))
-while followee_str in followee_arr || followee_str in follower_arr:
-	followee_str = "b" + str(random.randint(0,99))
 
 for i in range(6):
-	follower_str = "b" + str(random.randint(0,99))
-	while follower_str in follower_arr:
-		follower_str = "b" + str(random.randint(0,99))
-	follower_arr.append(follower_str)
-	followee_arr.append(followee_str)
-	followee_str = follower_str
-
-
-followee_str = "b" + str(random.randint(0,99))
-while followee_str in followee_arr || followee_str in follower_arr:
-	followee_str = "b" + str(random.randint(0,99))
-for i in range(6):
-	follower_str = "b" + str(random.randint(0,99))
-	while follower_str in follower_arr:
-		follower_str = "b" + str(random.randint(0,99))
-	follower_arr.append(follower_str)
-	followee_arr.append(followee_str)
-
-userlist = User.objects.all()
-
-for i in range(0, len(followee_arr)-1):
-	for usern in userlist:
+	for usern in User.objects.all():
 		if usern.get_username() == follower_arr[i]:
 			follower_user = usern
 		if usern.get_username() == followee_arr[i]:
 			followee_user = usern
-	followdouble = Following(follower=follower_user, followee=followee_user, follow_date=timezone)
+	followdouble = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+	followdouble.save()
 	print followdouble.__str__()
 
 
-
-
-
-
-# now how to do the following logic?
