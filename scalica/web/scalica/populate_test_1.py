7a146726eefca07e5a867ca1729b6bcb8e7a80cf
ryehollister <ryehollister@gmail.com>
2017-12-15 17:25:57 -0500

    rye bug fix

--- scalica/web/scalica/populate_test_1.py
+++ scalica/web/scalica/populate_test_1.py
@@ -8,16 +8,20 @@ from django.utils import timezone
 from micro.models import Following
 import random
 
+username = "L0"
+password = "pass"
 
 User = get_user_model()
 
-user_a = User.objects.create_user(username="L0")
+user_a = User.objects.create_user(username=username)
 user_a.set_password("pass")
 user_a.save()
 
 assert authenticate(username=username,password=password)
 
-user_b = User.objects.create_user(username="R1")
+username = "R1"
+
+user_b = User.objects.create_user(username=username)
 user_b.set_password("pass")
 user_b.save()
 
