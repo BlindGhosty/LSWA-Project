20c1b51c60f6127aa5cd22387e319f19600f4130
ryehollister <ryehollister@gmail.com>
2017-12-15 17:17:31 -0500

    rye pop5

--- scalica/web/scalica/populate_test_5.py
+++ scalica/web/scalica/populate_test_5.py
@@ -19,9 +19,9 @@ password = "pass"
 
 user_name = "Yair"
 temp_user = User.objects.create_user(username=user_name)
-temp_user.set_password("josh_homme_innocent")
+temp_user.set_password("josh_homme_innocent!")
 temp_user.save()
-assert authenticate(username=user_name, password="josh_homme_innocent")
+assert authenticate(username=user_name, password="josh_homme_innocent!")
 
 user_name = "B"
 temp_user = User.objects.create_user(username=user_name)
@@ -48,6 +48,48 @@ temp_user.save()
 assert authenticate(username=user_name, password=password)
 
 user_name = "ZPRIME"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "Bubble0"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "Bubble1"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "Bubble2"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "Bubble3"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "FollowForFollow0"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "FollowForFollow1"
+temp_user = User.objects.create_user(username=user_name)
+temp_user.set_password(password)
+temp_user.save()
+assert authenticate(username=user_name, password=password)
+
+user_name = "FollowForFollow2"
 temp_user = User.objects.create_user(username=user_name)
 temp_user.set_password(password)
 temp_user.save()
@@ -115,6 +157,62 @@ newFollow.save()
 
 follower_user = user_array[5]
 followee_user = user_array[3]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Bubble0 -> Bubble1
+
+follower_user = user_array[6]
+followee_user = user_array[7]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Bubble0 -> Bubble2
+
+follower_user = user_array[6]
+followee_user = user_array[8]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Bubble1 -> Bubble2
+
+follower_user = user_array[7]
+followee_user = user_array[8]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Bubble2 -> Bubble1
+
+follower_user = user_array[8]
+followee_user = user_array[7]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Follower0 -> Yair
+
+follower_user = user_array[9]
+followee_user = user_array[0]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Follower1 -> Yair
+
+follower_user = user_array[10]
+followee_user = user_array[0]
+
+newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
+newFollow.save()
+
+# Follower2 -> Yair
+
+follower_user = user_array[11]
+followee_user = user_array[0]
 
 newFollow = Following(follower=follower_user, followee=followee_user, follow_date=timezone.now())
 newFollow.save()
